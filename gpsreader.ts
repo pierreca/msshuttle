b537692b4fe972053c8cac3ae05554c557181591
Pierre Cauchois <pierreca@microsoft.com>
2016-02-25 15:06:16 -0800

    Put GpsReader in a module

--- gpsreader.ts
+++ gpsreader.ts
@@ -3,71 +3,73 @@ var fs = require('fs');
 var serial = require('serialport');
 var debug = require('debug')('msshuttle:obd');
 
-class GpsPosition {
-    latitude: number;
-    longitude: number;
-    altitude: number; 
-}
-
-class GpsReader {
-    realData: boolean;
-    started: boolean;
-    status: GpsPosition;
-    serialPort: any;
-    
-    public constructor(realData: boolean, comPort:string) {
-        this.realData = realData;
-        if(this.realData) {
-            this.serialPort = new serial.SerialPort(comPort, { 
-                baudrate: 9600, 
-                parser: serial.parsers.readline('\r\n')
-            });
-        } 
-        this.started = false;
-        this.status = new GpsPosition();
+export module GpsReader {
+    export class GpsPosition {
+        latitude: number;
+        longitude: number;
+        altitude: number; 
     }
-    
-    public start() : void {
-        if (!this.started) {
-            this.started = true;
-            
+
+    export class GpsReader {
+        realData: boolean;
+        started: boolean;
+        status: GpsPosition;
+        serialPort: any;
+        
+        public constructor(realData: boolean, comPort:string) {
+            this.realData = realData;
             if(this.realData) {
-                this.serialPort.on('data', function (line) {
-                    try {
-                        var parsedData = nmea.parse(line);
+                this.serialPort = new serial.SerialPort(comPort, { 
+                    baudrate: 9600, 
+                    parser: serial.parsers.readline('\r\n')
+                });
+            } 
+            this.started = false;
+            this.status = new GpsPosition();
+        }
+        
+        public start() : void {
+            if (!this.started) {
+                this.started = true;
+                
+                if(this.realData) {
+                    this.serialPort.on('data', function (line) {
+                        try {
+                            var parsedData = nmea.parse(line);
+                            debug('Parsed: ' + JSON.stringify(parsedData));
+                            if (parsedData.hasOwnProperty('lat')) {
+                                this.status.latitude = parsedData.lat;
+                                this.status.longitude = parsedData.lon;
+                                this.status.altitude = parsedData.alt;
+                            }
+                        } catch (err) {
+                            debug('Could not parse: ' + line)
+                        }
+                    });
+                } else {
+                    var self = this;
+                    var fakeData = [];
+                    var lineReader = require('readline').createInterface({
+                        input: require('fs').createReadStream('fakegps.txt')
+                    });
+
+                    lineReader.on('line', function (line) {
+                        fakeData.push(line);
+                        console.log(line);
+                    });
+                    
+                    var i = 0;
+                    setInterval(function() {
+                        var parsedData = nmea.parse(fakeData[i]);
                         debug('Parsed: ' + JSON.stringify(parsedData));
                         if (parsedData.hasOwnProperty('lat')) {
-                            this.status.latitude = parsedData.lat;
-                            this.status.longitude = parsedData.lon;
-                            this.status.altitude = parsedData.alt;
+                            self.status.latitude = parsedData.lat / 100;
+                            self.status.longitude = parsedData.lon / 100;
+                            self.status.altitude = parsedData.alt;
                         }
-                    } catch (err) {
-                        debug('Could not parse: ' + line)
-                    }
-                });
-            } else {
-                var self = this;
-                var fakeData = [];
-                var lineReader = require('readline').createInterface({
-                    input: require('fs').createReadStream('fakegps.txt')
-                });
-
-                lineReader.on('line', function (line) {
-                    fakeData.push(line);
-                    console.log(line);
-                });
-                
-                var i = 0;
-                setInterval(function() {
-                    var parsedData = nmea.parse(fakeData[i]);
-                    debug('Parsed: ' + JSON.stringify(parsedData));
-                    if (parsedData.hasOwnProperty('lat')) {
-                        self.status.latitude = parsedData.lat / 100;
-                        self.status.longitude = parsedData.lon / 100;
-                        self.status.altitude = parsedData.alt;
-                    }
-                    i++;
-                }, 1000);
+                        i++;
+                    }, 1000);
+                }
             }
         }
     }
