9135e18693a68b84ee0a0be74e4381653e087b27
Pierre Cauchois <pierreca@microsoft.com>
2016-02-25 10:39:57 -0800

    Fake OBD data + gpsReader class

--- obd2reader.ts
+++ obd2reader.ts
@@ -1,39 +1,123 @@
-var Obd2 = require('obd2');
+var Obd2 = require('obd2')
+var debug = require('debug')('msshuttle:obd');
+var fs = require('fs');
+
+class VehicleStatus {
+    at: string;
+    rpm: number;
+    speed: number;
+    throttle: number;
+    engineLoad: number;
+    engineOilTemp: number;
+    engine_light: boolean;
+}
 
 class Obd2Reader {
-    public static main(): number {
-        console.log('OBD reader starting');
-        var reader = new Obd2({
-            delay: 1000,
-            device: "elm327",
-            serial: "usb",
-            baud: 115200,
-            port: "COM3"
-        });
-        
-        reader.on('dataParsed', function(data) {
-           console.log('data parsed!'); 
-            console.log(data);
-            console.log('--------------');
-        });
-        
-        reader.on('dataReceived', function (data) {
-            console.log('data received:');
-            console.log(data);
-            console.log('--------------');
-        });
-        
-        
-        reader.start(function () {
-            console.log('started');
-            reader.readPID('0c', '01', function (data) {
-                console.log('readPID callback');
-                console.log(data);
+    realData: boolean;
+    status: VehicleStatus;
+    reader: any;
+    constructor(realData: boolean, comPort: string) {
+        this.status = new VehicleStatus();
+        this.realData = realData;
+        if (realData) {
+            this.reader = new Obd2({
+                delay: 1000,
+                device: "elm327",
+                serial: "usb",
+                baud: 115200,
+                port: comPort
             });
-        });
-        
-        return 0;
+            
+            this.reader.on('dataParsed', function(data) {
+            debug('data parsed: ' + data);
+            });
+            
+            this.reader.on('dataReceived', function (data) {
+                debug('data received: ' + data);
+            });
+            
+            // RPMs
+            this.reader.readPID('0c', '01', function (data) {
+                debug('readPID: [PID = ' + data + '][MODE = ' + data + '][DATA = ' + data + ']');
+                this.status.rpm = parseInt(data.value); 
+            });
+            
+            // Speed
+            this.reader.readPID('0d', '01', function (data) {
+                debug('readPID: [PID = ' + data + '][MODE = ' + data + '][DATA = ' + data + ']');
+                this.status.speed = parseInt(data.value); 
+            }); 
+            
+            // Throttle
+            this.reader.readPID('5a', '01', function (data) {
+                debug('readPID: [PID = ' + data + '][MODE = ' + data + '][DATA = ' + data + ']');
+                this.status.throttle = parseInt(data.value); 
+            });
+            
+            // Engine load
+            this.reader.readPID('04', '01', function (data) {
+                debug('readPID: [PID = ' + data + '][MODE = ' + data + '][DATA = ' + data + ']');
+                this.status.engineLoad = parseInt(data.value); 
+            });
+            
+            // Engine oil temp
+            this.reader.readPID('5c', '01', function (data) {
+                debug('readPID: [PID = ' + data + '][MODE = ' + data + '][DATA = ' + data + ']');
+                this.status.engineOilTemp = parseInt(data.value); 
+            });
+        }
+    }
+    
+    public start(): void {
+        debug('OBD reader starting');
+        if (this.realData) {
+            this.reader.start(function () {
+                debug('OBD reader started.'); 
+            });
+        } else {
+            var self = this;
+            self.status.rpm = 600;
+            
+            setInterval(function () {
+                var rpmVariation = Math.floor((Math.random() - 0.5) * 1000);
+                var throttleVariation = Math.floor(Math.random() * 100);
+                
+                self.status.at = new Date().toISOString();
+                self.status.rpm += rpmVariation;
+                self.status.throttle = throttleVariation;
+                
+                if (self.status.rpm < 600) {
+                    self.status.rpm = 600;
+                    self.status.engine_light = true;
+                } else {
+                    self.status.engine_light = false;
+                }
+                
+                if (self.status.rpm > 5000) {
+                    self.status.rpm = 5000;
+                    self.status.engine_light = true;
+                } else {
+                    self.status.engine_light = false;
+                }
+                
+                if (self.status.throttle < 0) self.status.throttle = 0;
+                if (self.status.throttle > 100) self.status.throttle = 100;
+                                
+            }, 1000);
+        }
+    }
+    
+    public getStatus() : VehicleStatus {
+        return this.status;
     }
 }
 
-Obd2Reader.main();
+var obdReader = new Obd2Reader(false, "COM4");
+obdReader.start();
+
+setInterval(function() {
+    var status = obdReader.getStatus();
+    fs.appendFile('obd2.logs', JSON.stringify(status) + '\r\n', function() {
+        debug('Status logged at ' + status.at);
+    })
+}, 1000);
