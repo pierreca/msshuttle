7112ae0dbe7547f5e68b1ddc2d693507dbb45210
Pierre Cauchois <pierreca@microsoft.com>
2016-02-25 15:01:37 -0800

    Fake GPS

--- gpsreader.ts
+++ gpsreader.ts
@@ -1,4 +1,5 @@
 var nmea = require('nmea');
+var fs = require('fs');
 var serial = require('serialport');
 var debug = require('debug')('msshuttle:obd');
 
@@ -9,16 +10,19 @@ class GpsPosition {
 }
 
 class GpsReader {
+    realData: boolean;
     started: boolean;
     status: GpsPosition;
     serialPort: any;
     
-    public constructor(comPort:string) {
-        this.serialPort = new serial.SerialPort(comPort, { 
-            baudrate: 9600, 
-            parser: serial.parsers.readline('\r\n')
-        });
-        
+    public constructor(realData: boolean, comPort:string) {
+        this.realData = realData;
+        if(this.realData) {
+            this.serialPort = new serial.SerialPort(comPort, { 
+                baudrate: 9600, 
+                parser: serial.parsers.readline('\r\n')
+            });
+        } 
         this.started = false;
         this.status = new GpsPosition();
     }
@@ -26,23 +30,45 @@ class GpsReader {
     public start() : void {
         if (!this.started) {
             this.started = true;
-            this.serialPort.on('data', function (line) {
-                try {
-                    var parsedData = nmea.parse(line);
+            
+            if(this.realData) {
+                this.serialPort.on('data', function (line) {
+                    try {
+                        var parsedData = nmea.parse(line);
+                        debug('Parsed: ' + JSON.stringify(parsedData));
+                        if (parsedData.hasOwnProperty('lat')) {
+                            this.status.latitude = parsedData.lat;
+                            this.status.longitude = parsedData.lon;
+                            this.status.altitude = parsedData.alt;
+                        }
+                    } catch (err) {
+                        debug('Could not parse: ' + line)
+                    }
+                });
+            } else {
+                var self = this;
+                var fakeData = [];
+                var lineReader = require('readline').createInterface({
+                    input: require('fs').createReadStream('fakegps.txt')
+                });
+
+                lineReader.on('line', function (line) {
+                    fakeData.push(line);
+                    console.log(line);
+                });
+                
+                var i = 0;
+                setInterval(function() {
+                    var parsedData = nmea.parse(fakeData[i]);
                     debug('Parsed: ' + JSON.stringify(parsedData));
                     if (parsedData.hasOwnProperty('lat')) {
-                        this.status.latitude = parsedData.lat;
-                        this.status.longitude = parsedData.lon;
-                        this.status.altitude = parsedData.alt;
+                        self.status.latitude = parsedData.lat / 100;
+                        self.status.longitude = parsedData.lon / 100;
+                        self.status.altitude = parsedData.alt;
                     }
-                } catch (err) {
-                    debug('Could not parse: ' + line)
-                }
-            });
+                    i++;
+                }, 1000);
+            }
         }
     }
 }
-
-var gpsReader = new GpsReader('COM7');
-gpsReader.start();
-
